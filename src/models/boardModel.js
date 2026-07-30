b053630db133dc85ddb60d7e37872de1a0a26b55
Vũ Mạnh Quỳnh <vumanhquynh2852005@gmail.com>
2026-07-30 09:12:24 +0700

    api createdBoard

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -1,4 +1,7 @@
 import Joi from 'joi'
+import {
+  GET_DB
+} from '~/config/database'
 const BOARD_COLLECTION_NAME = 'boards'
 const BOARD_COLLECTION_SCHEMA = Joi.object({
   title: Joi.string()
@@ -25,7 +28,28 @@ const BOARD_COLLECTION_SCHEMA = Joi.object({
   _destroy: Joi.boolean().default(false)
 
 })
+const createNew = async (data) => {
+  try {
+    const createdBoard = await GET_DB().collection(BOARD_COLLECTION_NAME).insertOne(data)
+    return createdBoard
+  } catch (error) {
+    throw new Error(error)
+
+  }
+}
+const findOneById = async (id) => {
+  try {
+    const result = await GET_DB().collection(BOARD_COLLECTION_NAME).findOne({
+      _id: id
+    })
+    return result
+  } catch (error) {
+    throw new Error(error)
+  }
+}
 export const boardModel = {
   BOARD_COLLECTION_NAME,
-  BOARD_COLLECTION_SCHEMA
+  BOARD_COLLECTION_SCHEMA,
+  createNew,
+  findOneById
 }
--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,14 +1,19 @@
 import slugify from '../untils/formatter'
-
+import {
+  boardModel,
+  findOneById
+} from '~/models/boardModel'
 const createNew = async (reqBody) => {
+  // eslint-disable-next-line no-useless-catch
   try {
-
     const newBoard = {
       ...reqBody,
       slug: slugify(reqBody.title)
     }
-    console.log(newBoard)
-    return newBoard
+    const createdBoard = await boardModel.createNew(newBoard)
+
+    const getNewBoard = await boardModel.findOneById(createdBoard.insertedId)
+    return getNewBoard
   } catch (error) {
     throw error
   }
