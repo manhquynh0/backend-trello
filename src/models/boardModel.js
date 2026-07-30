31c8df6daf9308a163a8b2f52cbaab7cab5de774
Vũ Mạnh Quỳnh <vumanhquynh2852005@gmail.com>
2026-07-30 08:31:57 +0700

    model

---
+++ src/models/boardModel.js
@@ -0,0 +1,31 @@
+import Joi from 'joi'
+const BOARD_COLLECTION_NAME = 'boards'
+const BOARD_COLLECTION_SCHEMA = Joi.object({
+  title: Joi.string()
+    .required()
+    .min(3)
+    .max(50)
+    .trim()
+    .strict(),
+  slug: Joi.string()
+    .required()
+    .min(3)
+    .max(50)
+    .trim()
+    .strict(),
+  description: Joi.string()
+    .required()
+    .min(3)
+    .max(256)
+    .trim()
+    .strict(),
+  columnOrderIds: Joi.array().items(Joi.string()).default([]),
+  createdAt: Joi.date().default(Date.now),
+  updatedAt: Joi.date().default(Date.now),
+  _destroy: Joi.boolean().default(false)
+
+})
+export const boardModel = {
+  BOARD_COLLECTION_NAME,
+  BOARD_COLLECTION_SCHEMA
+}
